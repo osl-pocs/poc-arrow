2c49b000d2987d28f9e650d9bf8a75a74392809c
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-07-13 20:34:02 -0400

    Add meson as a build option

--- arrow-c/src/examples-scalars.c
+++ arrow-c/src/examples-scalars.c
@@ -4,11 +4,17 @@
 #include <arrow-glib/scalar.h>
 
 #include "utils.h"
-// GArrowDate64Scalar *
-// garrow_date64_scalar_new(gint64 value)
+
+void example_boolean_scalar_new() {
+  GArrowBooleanScalar* v1 = garrow_boolean_scalar_new((gboolean)1);
+}
 
 void main_examples_scalars() {
   print_header("Scalars");
+
+  // boolean
+  example_boolean_scalar_new();
+
   // integer
 
   printf("\n> examples scalars done!\n\n");
--- arrow-c/src/main.c
+++ arrow-c/src/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-#include "examples_scalars.h"
-#include "examples_values.h"
+#include "examples-arrays.h"
+#include "examples-scalars.h"
+#include "examples-values.h"
+
+#define PROJECT_NAME "arrow-c"
 
 int main(int argc) {
-  // integer
   main_examples_values();
   main_examples_scalars();
+  main_examples_arrays();
 
   printf("PoC Arrow C done!\n");
   return 0;
