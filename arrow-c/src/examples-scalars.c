31285d46027abd0ef85122200c0dea3c1dd86ef2
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2023-02-28 01:11:59 -0400

    Add new scalar examples

--- arrow-c/src/examples-scalars.c
+++ arrow-c/src/examples-scalars.c
@@ -9,11 +9,19 @@ void example_boolean_scalar_new() {
   GArrowBooleanScalar* v1 = garrow_boolean_scalar_new((gboolean)1);
 }
 
+void example_i32_scalar_new() {
+  gint32 myIntValue = 42;
+  GArrowInt32Scalar* myIntScalar = garrow_int32_scalar_new(myIntValue);
+  gint32 myIntResult = garrow_int32_scalar_get_value(myIntScalar);
+  printf("The scalar integer value is: %d\n", myIntResult);
+}
+
 void main_examples_scalars() {
   print_header("Scalars");
 
   // boolean
   example_boolean_scalar_new();
+  example_i32_scalar_new();
 
   // integer
 
